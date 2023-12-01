3a26efa2fbb20eed333d3ab08a156ce6ed27642e
Ivona Cvija <123738999+IvonaCvija@users.noreply.github.com>
2023-12-01 17:43:01 +0000

    Update create.js

--- src/components/create.js
+++ src/components/create.js
@@ -1,37 +1,45 @@
+// Import necessary React hooks and libraries
 import { useState } from "react";
 import axios from "axios";
 
+// Functional component for the "Create" page
 function Create() {
-
+    // Use the useState hook to manage state for title, authors, and cover
     const [title, setTitle] = useState('');
     const [authors, setAuthors] = useState('');
     const [thumbnailUrl, setCover] = useState('');
 
+    // Function to handle form submission
     const handleSubmit = (e) => {
         e.preventDefault();
-
+        
+        // Log the entered values to the console
         console.log("Title: " + title +
             " Cover: " + thumbnailUrl +
             " Authors: " + authors);
 
+        // Create a book object with the entered values
         const book = {
             title: title,
             cover: thumbnailUrl,
             author: authors
         }
 
-        //makes a http request and output to console browser
+        / Make a POST request to the server to add the book
         axios.post('http://localhost:4000/api/book/', book)
+            // Do something on successful request, if needed
             .then()
+            // Handle errors, if any
             .catch();
     }
 
+    // Render the component with a form for user input
     return (
         <div>
             <h2>Hello from create component!</h2>
-            {/* form of type submit, invoke onSubmit when button is clicked */}
+            {/* Form for adding a new book */}
             <form onSubmit={handleSubmit}>
-                {/* taking user input for title */}
+                {/* Input field for book title */}
                 <div className="form-group">
                     <label>Add Book Title: </label>
                     <input type="text"
@@ -40,7 +48,7 @@ function Create() {
                         onChange={(e) => { setTitle(e.target.value) }}
                     />
                 </div>
-                {/* taking user input for authors */}
+                {/* Input field for book authors */}
                 <div className="form-group">
                     <label>Add Book Authors: </label>
                     <input type="text"
@@ -49,7 +57,7 @@ function Create() {
                         onChange={(e) => { setAuthors(e.target.value) }}
                     />
                 </div>
-                {/* taking user input for cover */}
+                {/* Input field for book cover */}
                 <div className="form-group">
                     <label>Add Cover: </label>
                     <input type="text"
@@ -58,7 +66,7 @@ function Create() {
                         onChange={(e) => { setCover(e.target.value) }}
                     />
                 </div>
-                {/* submitting it somewhere */}
+                {/* Submit button for adding the book */}
                 <div>
                     <input></input><input type="submit" value="Add Book" />
                 </div>
@@ -66,4 +74,5 @@ function Create() {
         </div>
     );
 }
+// Export the Create component as the default export
 export default Create;
