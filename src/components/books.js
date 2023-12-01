4f9a98ee97fe56ecb6a0979288a28ca212bc7693
Ivona Cvija <123738999+IvonaCvija@users.noreply.github.com>
2023-12-01 17:37:25 +0000

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,11 +1,16 @@
+// Import the BookItems component
 import BookItems from "./bookItems";
 
+// Functional component to render a list of books
 function Books(props) {
 
+    // Map through the array of books and render a BookItems component for each book
     return props.myBooks.map(
         (book) => {
+            // Pass the individual book data and key to the BookItems component
             return <BookItems myBook={book} key={book._id} reload={() => { props.Reload(); }}></BookItems>
         }
     );
 }
+// Export the Books component as the default export
 export default Books;
